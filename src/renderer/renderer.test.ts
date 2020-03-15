c8a17c3db0c1779e6f8048d687262a396387cf2b
Myriad-Dreamin <camiyoru@gmail.com>
2020-03-16 02:03:26 +0800

    add new test for std markdown rules

--- src/renderer/renderer.test.ts
+++ src/renderer/renderer.test.ts
@@ -1,6 +1,6 @@
 import {Renderer} from "./renderer";
 import {Parser, StringStream} from "..";
-import {latex_md, math_md, test_md} from "../lib/data.test";
+import {latex_md, math_md} from "../lib/data.test";
 import {newRules} from "../rules";
 
 
@@ -20,36 +20,17 @@ describe('renderer', () => {
         ));
     });
 
-    it('should render test.md', () => {
-        console.log(new Renderer(new Parser(newRules({enableLaTeX: true})), {enableLaTeX: true}).render(
-            new StringStream(test_md),
-        ));
-    });
+    // it('should render test.md', () => {
+    //     console.log(new Renderer(new Parser(newRules({enableLaTeX: true})), {enableLaTeX: true}).render(
+    //         new StringStream(test_md),
+    //     ));
+    // });
 
     it('should render code block with escape', () => {
         console.log(new Renderer(new Parser(newRules({enableLaTeX: true})), {enableLaTeX: true}).render(
             new StringStream('    <div class="footer">\n' +
                 '        &copy; 2004 Foo Corporation\n' +
                 '    </div>'),
-        ));
-    });
-
-    it('should render code block with highlight callback', () => {
-        console.log(new Renderer(new Parser(newRules({enableLaTeX: true})), {
-            enableLaTeX: true,
-            highlight: (code, language) => {
-                if (language) {
-                    return '<div class="rendered">' + code + '</div>'
-                } else {
-                    return code
-                }
-            }
-        }).render(
-            new StringStream('```html\n' +
-                '<div class="footer">\n' +
-                '    &copy; 2004 Foo Corporation\n' +
-                '</div>\n' +
-                '```'),
         ));
     });
 
