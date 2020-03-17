88d8f5c5319c4d99e742f41b6a012607e4a2d2a2
Myriad-Dreamin <camiyoru@gmail.com>
2020-03-17 09:14:22 +0800

    add call escapeHTML in renderer.render

--- src/lib/escape.ts
+++ src/lib/escape.ts
@@ -5,22 +5,3 @@ export function escapeHTML(s: string) {
         .replace(/>/g, '&gt;')
         .replace(/"/g, '&quot;');
 }
-
-const escapeChars = "\\`_*!#+-.[]{}()";
-
-export function unescapeBackSlash(s: string): string {
-    let res = "", q = 0, la: string = 'a';
-    for (let i = 0; i < s.length; i++) {
-        if (la === '\\' && escapeChars.includes(s[i])) {
-            res += s.slice(q, i - 1);
-            q = i;
-            la = 'a';
-        } else {
-            la = s[i];
-        }
-    }
-    if (q < s.length) {
-        return res + s.slice(q);
-    }
-    return res
-}
