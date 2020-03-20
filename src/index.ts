a0dbb4c0ad6fd88704629e83bbb5f9983b468a37
Myriad-Dreamin <camiyoru@gmail.com>
2020-03-20 15:40:48 +0800

    fix interface options

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
-import {Parser, ParserOptions} from "./parser/parser";
-import {HighlightFunc, Renderer, RenderMiddleware, RenderOptions} from "./renderer/renderer";
+import {Parser} from "./parser/parser";
+import {HighlightFunc, Renderer, RenderMiddleware} from "./renderer/renderer";
 import {StringStream} from "./lib/stream";
 import {newBlockRules, newInlineRules, newRules} from "./rules";
 import {HTMLBlockOptions} from "./rules/std";
 
-interface Options {
+export interface MarkTeXParserOptions {
     enableLaTeX?: boolean;
     enableGFMRules?: boolean;
 
     enableHtml?: boolean;
     HTMLBlockOptions?: HTMLBlockOptions;
+}
+
+
+export interface MarkTeXRendererOptions extends MarkTeXParserOptions {
+    parser?: Parser
 
     highlight?: HighlightFunc;
     wrapCodeClassTag?: (language: string) => string;
@@ -26,11 +31,11 @@ interface Options {
 // noinspection JSUnusedGlobalSymbols
 const myriad = {
     author: "Myriad-Dreamin",
-    newParser(options?: Options): Parser {
+    newParser(options?: MarkTeXParserOptions): Parser {
         return new Parser(newRules(options));
     },
-    newRenderer(options?: Options): Renderer {
-        return new Renderer(myriad.newParser(options), options);
+    newRenderer(options?: MarkTeXRendererOptions): Renderer {
+        return new Renderer(options?.parser || myriad.newParser(options), options);
     },
     newStringStream(str: string): StringStream {
         return new StringStream(str);
@@ -42,4 +47,4 @@ const myriad = {
 
 // noinspection JSUnusedGlobalSymbols
 export default myriad;
-export {myriad, Options, Parser, Renderer, StringStream};
+export {myriad, Parser, Renderer, StringStream};
