e6d61348c60badb9ed8f56b95c7aa79531c3d0e7
Myriad-Dreamin <camiyoru@gmail.com>
2021-03-15 21:41:47 +0800

    fix: bench compile error

--- src/renderer/renderer.bench.ts
+++ src/renderer/renderer.bench.ts
@@ -1,8 +1,8 @@
-import {createContext} from "../lib/test_util";
 import {Parser, StringStream} from "..";
 import {cn_book_md, math_md} from "../lib/data.test";
 import {newRules} from "../rules";
 import {RenderDriver} from "../driver/driver";
+import {Renderer} from "./renderer";
 import Benchmark = require("benchmark");
 
 let suite = new Benchmark.Suite;
@@ -10,18 +10,18 @@ let suite = new Benchmark.Suite;
 // renderer cn_book_md x 656 ops/sec ±1.06% (94 runs sampled)
 
 suite.add('render math_md', () => {
-    new RenderDriver(createContext()).render(
+    new RenderDriver(new Parser(), new Renderer()).render(
         new StringStream(math_md),
     );
 });
 
 suite.add('render cn_book_md', () => {
-    new RenderDriver(createContext(), {}).render(
+    new RenderDriver(new Parser(), new Renderer(), {}).render(
         new StringStream(cn_book_md),
     );
 });
 
-let latexRenderer = new RenderDriver(new Parser(newRules({enableLaTeX: true})), {enableLaTeX: true});
+let latexRenderer = new RenderDriver(new Parser(newRules({enableLaTeX: true})), new Renderer({enableLaTeX: true}));
 
 // render math_md with latex option x 79,236 ops/sec ±1.12% (95 runs sampled)
 // render cn_book_md with latex option x 610 ops/sec ±0.94% (92 runs sampled)
